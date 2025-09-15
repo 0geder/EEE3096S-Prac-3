44dbea80efb860e8510b181d47be9afc9101c7bd
0geder <0gedasam0k@gmail.com>
2025-09-15 20:32:06 +0200

    Add syscalls.c with minimal stubs to satisfy linker
    Implements _write, _read, _close, _lseek, etc.
    Prevents build errors from missing system calls.

--- Practical_3A/Core/Src/main.c
+++ Practical_3A/Core/Src/main.c
@@ -31,7 +31,10 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+int _close(int file) { (void)file; return -1; }
+int _lseek(int file, int ptr, int dir) { (void)file; (void)ptr; (void)dir; return 0; }
+int _read(int file, char *ptr, int len) { (void)file; (void)ptr; (void)len; return 0; }
+int _write(int file, char *ptr, int len) { (void)file; (void)ptr; return len; }
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
